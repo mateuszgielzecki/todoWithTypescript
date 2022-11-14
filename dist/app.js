e3dff0cbebaff9eea108bc9704e0c53720506893
Mateusz Giełżecki <mateuszgielzecki@gmail.com>
2022-11-14 22:18:36 +0100

    add checkbox functionality

--- dist/app.js
+++ dist/app.js
@@ -1,25 +1,55 @@
-// <li>
-//       <label for="task-1">Wyrzucić śmieci</label>
-//       <input type="checkbox" id="task-1" name="Wyrzucić śmieci">
-//     </li>
 const taskContainerElement = document.querySelector(".tasks");
 const taskNameInputElement = document.querySelector("#name");
 const addButtonElement = document.querySelector("button");
-const tasks = ["Wyrzucić śmieci", "Pójść na siłkę", "Nakramić koty"];
+const categories = ["general", "work", "gym", "hobby"];
+const tasks = [
+    {
+        name: "Wyrzucić śmieci",
+        done: false,
+        category: "hobby",
+    },
+    {
+        name: "Pójść na siłkę",
+        done: true,
+        category: "gym",
+    },
+    {
+        name: "Nakramić koty",
+        done: false,
+        category: "work",
+    },
+];
 const render = () => {
     taskContainerElement.innerHTML = "";
-    tasks.forEach((task) => {
+    tasks.forEach((task, index) => {
         const taskElement = document.createElement("li");
-        taskElement.innerText = task;
+        if (task.category) {
+            taskElement.classList.add(task.category);
+        }
+        const id = `task-${index}`;
+        const labelElement = document.createElement("label");
+        labelElement.innerText = task.name;
+        labelElement.setAttribute("for", id);
+        const checkboxElement = document.createElement("input");
+        checkboxElement.type = "checkbox";
+        checkboxElement.name = task.name;
+        checkboxElement.id = id;
+        checkboxElement.checked = task.done;
+        checkboxElement.addEventListener("change", () => {
+            task.done = !task.done;
+        });
+        taskElement.appendChild(labelElement);
+        taskElement.appendChild(checkboxElement);
         taskContainerElement.appendChild(taskElement);
     });
 };
 const addTask = (task) => {
     tasks.push(task);
 };
-render();
 addButtonElement.addEventListener("click", (e) => {
     e.preventDefault();
-    addTask(taskNameInputElement.value);
+    addTask({ name: taskNameInputElement.value, done: false });
     render();
 });
+addTask({ name: "zrobić", category: "gym", done: false });
+render();
--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,72 @@
-// <li>
-//       <label for="task-1">Wyrzucić śmieci</label>
-//       <input type="checkbox" id="task-1" name="Wyrzucić śmieci">
-//     </li>
-
 const taskContainerElement: HTMLElement = document.querySelector(".tasks");
 const taskNameInputElement: HTMLInputElement = document.querySelector("#name");
 const addButtonElement: HTMLButtonElement = document.querySelector("button");
 
-const tasks: string[] = ["Wyrzucić śmieci", "Pójść na siłkę", "Nakramić koty"];
+interface Task {
+  name: string;
+  done: boolean;
+  category?: "general" | "gym" | "hobbie" | "work";
+}
+
+const categories: string[] = ["general", "work", "gym", "hobby"];
+
+const tasks: Task[] = [
+  {
+    name: "Wyrzucić śmieci",
+    done: false,
+    category: "hobby",
+  },
+  {
+    name: "Pójść na siłkę",
+    done: true,
+    category: "gym",
+  },
+  {
+    name: "Nakramić koty",
+    done: false,
+    category: "work",
+  },
+];
 
 const render = () => {
   taskContainerElement.innerHTML = "";
-  tasks.forEach((task) => {
+  tasks.forEach((task, index) => {
     const taskElement: HTMLElement = document.createElement("li");
-    taskElement.innerText = task;
+
+    if (task.category) {
+      taskElement.classList.add(task.category);
+    }
+
+    const id: string = `task-${index}`;
+    const labelElement: HTMLLabelElement = document.createElement("label");
+    labelElement.innerText = task.name;
+    labelElement.setAttribute("for", id);
+
+    const checkboxElement: HTMLInputElement = document.createElement("input");
+    checkboxElement.type = "checkbox";
+    checkboxElement.name = task.name;
+    checkboxElement.id = id;
+    checkboxElement.checked = task.done;
+    checkboxElement.addEventListener("change", () => {
+      task.done = !task.done;
+    });
+
+    taskElement.appendChild(labelElement);
+    taskElement.appendChild(checkboxElement);
+
     taskContainerElement.appendChild(taskElement);
   });
 };
 
-const addTask = (task: string) => {
+const addTask = (task: Task) => {
   tasks.push(task);
 };
 
-render();
-
 addButtonElement.addEventListener("click", (e: Event) => {
   e.preventDefault();
-  addTask(taskNameInputElement.value);
+  addTask({ name: taskNameInputElement.value, done: false });
   render();
 });
+
+addTask({ name: "zrobić", category: "gym", done: false });
+render();
